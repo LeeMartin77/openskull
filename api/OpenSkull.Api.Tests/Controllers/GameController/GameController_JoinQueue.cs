1876cd8b92a6805fa0ee9c7ea34d1928a71d436a
Lee <hello@leejohnmartin.co.uk>
2022-06-30 21:46:55 +0100

    At least somewhat shoring up our queue controller tests

--- api/OpenSkull.Api.Tests/Controllers/GameController/GameController_JoinQueue.cs
+++ api/OpenSkull.Api.Tests/Controllers/GameController/GameController_JoinQueue.cs
@@ -13,17 +13,22 @@ namespace OpenSkull.Api.Tests;
 [TestClass]
 public class GameController_JoinQueue {
   [TestMethod]
-  public async Task JoinQueue_HappyPath_AllRuns(){
+  public async Task JoinQueue_HappyPath_NullQueueResult_ReturnsNoContent(){
     // Arrange
     var playerId = Guid.NewGuid();
-    
+    int gameSize = 3;
+
     var httpContext = new DefaultHttpContext();
     httpContext.Request.Headers["X-OpenSkull-UserId"] = playerId.ToString();
+    var mockGameQueue = new Mock<IGameCreationQueue>();
+    mockGameQueue.Setup(x => x.JoinGameQueue(playerId, gameSize)).ReturnsAsync(() => {
+      return null;
+    });
     
     var gameController = new GameController(
       new Mock<ILogger<GameController>>().Object,
       new Mock<IGameStorage>().Object,
-      new Mock<IGameCreationQueue>().Object,
+      mockGameQueue.Object,
       new Mock<GameCreateNew>().Object,
       new Mock<TurnPlayCard>().Object,
       new Mock<TurnPlaceBid>().Object,
@@ -36,9 +41,48 @@ public class GameController_JoinQueue {
     };
 
     // Act
-    var result = await gameController.JoinQueue(new GameController.GameQueueParameters { GameSize = 3 });
+    var result = await gameController.JoinQueue(new GameController.GameQueueParameters { GameSize = gameSize });
 
     // Assert
     Assert.AreEqual(typeof(NoContentResult), result.Result!.GetType());
+    mockGameQueue.Verify(x => x.JoinGameQueue(playerId, gameSize), Times.Once);
+  }
+
+  [TestMethod]
+  public async Task JoinQueue_HappyPath_GameQueueResult_ReturnsPlayerGame(){
+    // Arrange
+    var playerId = Guid.NewGuid();
+    int gameSize = 3;
+    
+    var httpContext = new DefaultHttpContext();
+    httpContext.Request.Headers["X-OpenSkull-UserId"] = playerId.ToString();
+    var mockGameQueue = new Mock<IGameCreationQueue>();
+    mockGameQueue.Setup(x => x.JoinGameQueue(playerId, gameSize))
+      .ReturnsAsync(new GameStorage{ 
+        Id = Guid.NewGuid(), 
+        Game = GameFunctions.CreateNew(new Guid[3] { playerId, Guid.NewGuid(), Guid.NewGuid() }).Value 
+        });
+
+    var gameController = new GameController(
+      new Mock<ILogger<GameController>>().Object,
+      new Mock<IGameStorage>().Object,
+      mockGameQueue.Object,
+      new Mock<GameCreateNew>().Object,
+      new Mock<TurnPlayCard>().Object,
+      new Mock<TurnPlaceBid>().Object,
+      new Mock<TurnFlipCard>().Object
+    ){ 
+      ControllerContext = new ControllerContext()
+      {
+          HttpContext = httpContext
+      }
+    };
+
+    // Act
+    var result = await gameController.JoinQueue(new GameController.GameQueueParameters { GameSize = gameSize });
+
+    // Assert
+    Assert.AreEqual(typeof(PlayerGame), result.Value!.GetType());
+    mockGameQueue.Verify(x => x.JoinGameQueue(playerId, gameSize), Times.Once);
   }
 }
