458e8ca8fc54b58e9947b5494841961c88a2c8b5
Lee <hello@leejohnmartin.co.uk>
2022-07-23 22:16:30 +0100

    Refactor: starting to carve contol component

--- webapp/src/components/game/GameControlsComponent.tsx
+++ webapp/src/components/game/GameControlsComponent.tsx
@@ -5,10 +5,14 @@ import { CardState, CardType, PlayerGame, RoundPhase, TurnAction } from "../../m
 
 const SKIP_VALUE = -1;
 
-export function GameControlsComponent({ game }: { game: PlayerGame }) {
+interface IControlProps { 
+  game: PlayerGame, 
+  clicked: boolean,
+  setClicked: (i: boolean) => void
+}
+
+function GamePlayCardControlComponent({ game, clicked, setClicked }: IControlProps ) {
   const roundIndex = game.roundNumber - 1;
-  
-  const [clicked, setClicked] = useState(false);
 
   const playCard = (cardId: string) => {
     setClicked(true);
@@ -24,6 +28,26 @@ export function GameControlsComponent({ game }: { game: PlayerGame }) {
       .finally(() => setClicked(false))
   }
 
+  return <>{game.playerCards.map(card => {
+    return <Button
+    key={card.id}
+    onClick={() => playCard(card.id)}
+    color={card.state === CardState.Discarded ? "error" : "primary"}
+    disabled={
+      ![RoundPhase.PlayFirstCards, RoundPhase.PlayCards].includes(game.currentRoundPhase) || 
+      clicked || game.activePlayerIndex !== game.playerIndex || 
+      card.state === CardState.Discarded || 
+      game.playerRoundCardIdsPlayed[roundIndex].includes(card.id)
+    }
+    >
+      Play {CardType[card.type]}
+    </Button>
+  })}</>
+}
+
+function GamePlaceBidControlComponent({ game, clicked, setClicked }: IControlProps) {
+  const roundIndex = game.roundNumber - 1;
+
   const placebid = (bid: number) => {
     setClicked(true);
     fetch(`${API_ROOT_URL}/games/${game.id}/turn`, 
@@ -37,7 +61,30 @@ export function GameControlsComponent({ game }: { game: PlayerGame }) {
       })
       .finally(() => setClicked(false))
   }
-  
+
+  const maxBid = game.roundCountPlayerCardsPlayed[roundIndex].reduce((c, cc) => c + cc, 0);
+  const minBid = Math.max(...game.currentBids) + 1;
+
+  const arrayOfValues = [];
+  for (let i = minBid; i <= maxBid; i++) {
+    arrayOfValues.push(i);
+  }
+
+  return <><Button 
+    disabled={clicked || game.activePlayerIndex !== game.playerIndex || game.currentRoundPhase === RoundPhase.PlayFirstCards || minBid === 1} 
+    onClick={() => placebid(SKIP_VALUE)}>Retire</Button>
+  {//This is a completely terrible UI 
+  arrayOfValues.map((bidNumber, i) => {
+    return <Button key={i}
+    disabled={clicked || game.activePlayerIndex !== game.playerIndex  || game.currentRoundPhase === RoundPhase.PlayFirstCards} 
+    onClick={() => placebid(bidNumber)}>Bid {bidNumber}</Button>
+  })
+  }</>
+}
+
+function GameFlipCardControlComponent({ game, clicked, setClicked }: IControlProps) {
+  const roundIndex = game.roundNumber - 1;
+
   const flipCard = (targetPlayerIndex: number) => {
     setClicked(true);
     fetch(`${API_ROOT_URL}/games/${game.id}/turn`, 
@@ -52,57 +99,30 @@ export function GameControlsComponent({ game }: { game: PlayerGame }) {
       .finally(() => setClicked(false))
   }
 
-  const maxBid = game.roundCountPlayerCardsPlayed[roundIndex].reduce((c, cc) => c + cc, 0);
-  const minBid = Math.max(...game.currentBids) + 1;
-
-  const arrayOfValues = [];
-  for (let i = minBid; i <= maxBid; i++) {
-    arrayOfValues.push(i);
-  }
-
-  const cardButtons = game.playerCards.map(card => {
+  return <>{game.roundCountPlayerCardsPlayed[roundIndex].map((playedCount, i) => {
     return <Button
-    key={card.id}
-    onClick={() => playCard(card.id)}
-    color={card.state === CardState.Discarded ? "error" : "primary"}
+    key={i}
+    onClick={() => flipCard(i)}
     disabled={
-      ![RoundPhase.PlayFirstCards, RoundPhase.PlayCards].includes(game.currentRoundPhase) || 
-      clicked || game.activePlayerIndex !== game.playerIndex || 
-      card.state === CardState.Discarded || 
-      game.playerRoundCardIdsPlayed[roundIndex].includes(card.id)
+      clicked ||
+      (i !== game.activePlayerIndex && game.roundPlayerCardsRevealed[roundIndex][game.activePlayerIndex].length !== playedCount) ||
+      playedCount === game.roundPlayerCardsRevealed[roundIndex][i].length
     }
     >
-      Play {CardType[card.type]}
+      Flip Player {i}
     </Button>
-  })
+  })}</>
+}
 
-  const bidControls = <><Button 
-      disabled={clicked || game.activePlayerIndex !== game.playerIndex || game.currentRoundPhase === RoundPhase.PlayFirstCards || minBid === 1} 
-      onClick={() => placebid(SKIP_VALUE)}>Retire</Button>
-    {//This is a completely terrible UI 
-    arrayOfValues.map((bidNumber, i) => {
-      return <Button key={i}
-      disabled={clicked || game.activePlayerIndex !== game.playerIndex  || game.currentRoundPhase === RoundPhase.PlayFirstCards} 
-      onClick={() => placebid(bidNumber)}>Bid {bidNumber}</Button>
-    })
-    }</>
+export function GameControlsComponent({ game }: { game: PlayerGame }) {
   
-  const flipButtons = game.roundCountPlayerCardsPlayed[roundIndex].map((playedCount, i) => {
-      return <Button
-      key={i}
-      onClick={() => flipCard(i)}
-      disabled={
-        clicked ||
-        (i !== game.activePlayerIndex && game.roundPlayerCardsRevealed[roundIndex][game.activePlayerIndex].length !== playedCount) ||
-        playedCount === game.roundPlayerCardsRevealed[roundIndex][i].length
-      }
-      >
-        Flip Player {i}
-      </Button>
-    })
+  const [clicked, setClicked] = useState(false);
 
   if (game.currentRoundPhase !== RoundPhase.Flipping) {
-    return <>{cardButtons}{bidControls}</>
+    return <>
+      <GamePlayCardControlComponent game={game} clicked={clicked} setClicked={setClicked}/>
+      <GamePlaceBidControlComponent game={game} clicked={clicked} setClicked={setClicked}/>
+    </>
   }
-  return <>{flipButtons}</>;
+  return <GameFlipCardControlComponent game={game} clicked={clicked} setClicked={setClicked}/>;
 }
