5a13e259ee49132983c22c9f6486ae65d59add93
Lee <hello@leejohnmartin.co.uk>
2022-07-30 23:03:36 +0100

    Crude bid placing control

--- webapp/src/lib/components/game/GameControls.svelte
+++ webapp/src/lib/components/game/GameControls.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-  import { API_ROOT_URL } from 'src/config';
+  import { API_ROOT_URL, SKIP_VALUE } from 'src/config';
   import { generateUserHeaders } from 'src/stores/player';
   import { CardState, TurnAction, type PlayerGame } from 'src/types/Game';
+  import { prevent_default } from 'svelte/internal';
 
   export let game: PlayerGame;
   const roundIndex = game.roundNumber - 1;
@@ -12,6 +13,17 @@
       body: JSON.stringify({
         action: TurnAction[TurnAction.Card],
         cardId
+      })
+    });
+  };
+
+  const placeBid = (bid: number) => {
+    fetch(`${API_ROOT_URL}/games/${game.id}/turn`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', ...generateUserHeaders() },
+      body: JSON.stringify({
+        action: TurnAction[TurnAction.Bid],
+        bid
       })
     });
   };
@@ -28,6 +40,20 @@
             game.activePlayerIndex !== game.playerIndex ||
             game.playerRoundCardIdsPlayed[roundIndex].includes(card.id)}
           >{card.type}</button
+        >
+      {/each}
+    </div>
+    <h3>Place Bid</h3>
+    <div>
+      <button
+        on:click={() => placeBid(SKIP_VALUE)}
+        disabled={game.activePlayerIndex !== game.playerIndex}>Withdraw</button
+      >
+      {#each Array.from({ length: game.roundCountPlayerCardsPlayed[roundIndex].reduce((prev, curr) => prev + curr, 1) }, (v, i) => i) as bidNumber}
+        <button
+          on:click={() => placeBid(bidNumber)}
+          disabled={game.activePlayerIndex !== game.playerIndex}
+          >{bidNumber}</button
         >
       {/each}
     </div>
