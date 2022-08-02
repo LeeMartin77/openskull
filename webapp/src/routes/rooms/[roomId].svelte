8e0cfeed4674debe7792c72783e3e66ef055bfe2
Lee <hello@leejohnmartin.co.uk>
2022-08-02 21:17:59 +0100

    Update: room interface

--- webapp/src/routes/rooms/[roomId].svelte
+++ webapp/src/routes/rooms/[roomId].svelte
@@ -38,15 +38,37 @@
   });
 </script>
 
-<div>
-  <h2>You are in room {roomId}</h2>
-  <h4>Players:</h4>
-  {#if playersInRoom.length > 2 && playersInRoom.length < 7 && connection}
-    <button on:click={startgame}>Start game!</button>
+<div style="max-width: 80vw">
+  <h2 style="font-weight: 400">You are in room <span style="font-weight: 700">{roomId}</span></h2>
+  <a href="/rooms/{roomId}" class="room-link">Link to Room</a>
+  {#if playersInRoom.length < 3}
+    <p>Need {3 - playersInRoom.length} more player(s) to start a game.</p>
   {/if}
+  {#if playersInRoom.length > 6}
+    <p>Max game size of 6 exceeded! Need to lose {playersInRoom.length - 6} players to start game.</p>
+  {/if}
+  {#if playersInRoom.length > 2 && playersInRoom.length < 7 && connection}
+    <div>
+      <button class="start-button" on:click={startgame}>Start game!</button>
+    </div>
+  {/if}
+  <h4>Players:</h4>
   <ul>
     {#each playersInRoom as player}
       <li>{player}</li>
     {/each}
   </ul>
 </div>
+
+<style>
+  .room-link {
+    text-align: center;
+  }
+
+  .start-button {
+    padding: 0.5em;
+    width: 100%;
+    margin-top: 1em;
+    font-weight: 700;
+  }
+</style>
