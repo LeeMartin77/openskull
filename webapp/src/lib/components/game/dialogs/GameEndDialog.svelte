d76c45db5675c93021f17bf059aadf729dba0b25
Lee <hello@leejohnmartin.co.uk>
2022-08-02 22:00:46 +0100

    Added: Game Complete dialog styleup

--- webapp/src/lib/components/game/dialogs/GameEndDialog.svelte
+++ webapp/src/lib/components/game/dialogs/GameEndDialog.svelte
@@ -1,13 +1,26 @@
 <script lang="ts">
   import type { PublicGame } from 'src/types/Game';
   import Dialog from 'src/lib/components/dialog/Dialog.svelte';
+  import CardComponent from '../CardComponent.svelte';
 
   export let open: boolean = false;
   export let game: PublicGame;
 </script>
 
 <Dialog {open}>
-  <p>
-    {game.playerNicknames[game.activePlayerIndex]} won after {game.roundCountPlayerCardsPlayed.length} rounds
+  <h2 style="text-align: center;">{game.playerNicknames[game.activePlayerIndex]} Wins!</h2>
+  <div class="card-container">
+    <CardComponent display={'Flower'} />
+  </div>
+  <p style="text-align: center;">
+    After {game.roundCountPlayerCardsPlayed.length} rounds
   </p>
 </Dialog>
+
+<style>
+  .card-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
