29ce1f6eed949eb68f80838741d554a30f447b26
Lee Martin <lee.martin@pliminus.co.uk>
2022-08-13 15:02:54 +0100

    WebApp changes for new API route

--- webapp/src/config.ts
+++ webapp/src/config.ts
@@ -1,4 +1,4 @@
-export const API_ROOT_URL = import.meta.env.VITE_API_ROOT_URL;
+export const API_ROOT_URL = import.meta.env.VITE_API_ROOT_URL ?? '/api';
 export const USER_ID_HEADER = 'X-OpenSkull-UserId';
 export const USER_SECRET_HEADER = 'X-OpenSkull-UserSecret';
 export const SKIP_VALUE = -1;
--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -4,13 +4,23 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // @ts-ignore ts(1259)
 import path from 'node:path';
 
+const proxy = {
+  '/api': {
+       target: 'http://localhost:5248',
+       changeOrigin: true,
+       secure: false,      
+       ws: true,
+   }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   preview: {
     port: 3000
   },
   server: {
-    port: 3000
+    port: 3000,
+    proxy
   },
   resolve: {
     alias: {
